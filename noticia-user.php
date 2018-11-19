d8184738971e94a3769c2240b805156ca0ae6f24
Miguel <mpaivaluz@gmail.com>
2018-11-19 10:55:25 -0200

    teste

--- noticia-user.php
+++ noticia-user.php
@@ -160,7 +160,7 @@ mysqli_query ($con, "insert into comentarios ( coment,  idNoticia, date, hora )v
 
        <div id="footer">
 	<p class="legal">
-		&copy;2014, Centro Educacional Marista  <a href="http://www.solmarista.org.br/"style="text-decoration:none" target="_blank"><font color="#000000"><b>Irmão Acácio</b>
+		&copy;2014, Centro Educacional Marista teste  <a href="http://www.solmarista.org.br/"style="text-decoration:none" target="_blank"><font color="#000000"><b>Irmão Acácio</b>
 		&nbsp;&nbsp;&bull;&nbsp;&nbsp;
 		Desenvolvido por <a href="http://blogcinenoticias.blogspot.com.br/"style="text-decoration:none" target="_blank"><font color="#000000"><b>Lucas Lunardelli</b></a>
 		&nbsp;&nbsp;&bull;&nbsp;&nbsp;
